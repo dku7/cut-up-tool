b64af796dfe747d3ad3c25d80140e542c9c52250
dku7 <dku7@proton.me>
2025-01-24 09:49:38 +0000

    add getRandomLineLength and rename file to utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-export default function getRandomFont() {
+export function getRandomFont() {
   const fonts = [
     "font-mono",
     "font-serif",
@@ -8,4 +8,8 @@ export default function getRandomFont() {
   ];
 
   return fonts[Math.floor(Math.random() * fonts.length)];
+}
+
+export function getRandomLineLength() {
+  return Math.floor(Math.random() * 10) + 2;
 }
