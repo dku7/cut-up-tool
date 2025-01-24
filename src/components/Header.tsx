472c6783d1b6a64d36d785fc0ef762c106940433
dku7 <dku7@proton.me>
2025-01-24 08:03:18 +0000

    centre text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header>
+    <header className="text-center p-4">
       <h1 className="text-3xl">Cut-Up Text Generator</h1>
     </header>
   );
